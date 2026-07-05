b76500ec3c81bb57751916db2f48b567b49966a0
kush1411330 <kushagradeep14@gmail.com>
2026-07-05 09:01:34 +0530

    Add files via upload

---
+++ Dump20260705/propertydb_property.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.43, for Win64 (x86_64)
+--
+-- Host: localhost    Database: propertydb
+-- ------------------------------------------------------
+-- Server version	9.4.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `property`
+--
+
+LOCK TABLES `property` WRITE;
+/*!40000 ALTER TABLE `property` DISABLE KEYS */;
+INSERT INTO `property` VALUES (1,_binary '','IndiGo',40,'Delhi','Mumbai','2026-09-11',3814.00,'06:15:00','08:25:00'),(2,_binary '','Air India',35,'Delhi','Bangalore','2026-09-12',5240.00,'09:40:00','12:15:00'),(3,_binary '','Vistara',28,'Mumbai','Goa','2026-09-13',2890.00,'14:20:00','15:35:00'),(4,_binary '','IndiGo',32,'Hyderabad','Chennai','2026-09-14',3150.00,'11:05:00','12:15:00'),(5,_binary '','Air India',25,'Kolkata','Delhi','2026-09-15',6890.00,'17:30:00','20:00:00'),(6,_binary '','IndiGo',30,'Delhi','Pune','2026-10-02',3500.00,'08:00:00','10:00:00'),(7,_binary '','Akasa Air',50,'Bangalore','Delhi','2026-09-30',5876.00,'08:00:00','11:00:00'),(8,_binary '','akasa airline',1,'delhi','bombay','2026-09-26',3004.00,'06:00:00','08:00:00');
+/*!40000 ALTER TABLE `property` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-07-05  9:00:42
